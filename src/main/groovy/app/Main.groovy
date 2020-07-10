41f24dc46baf4e7f3081fa0ee4b81d553385c598
Aleks Tamarkin <atamarkin2@bloomberg.net>
2020-07-10 10:46:01 -0400

    initial

--- src/main/groovy/app/Main.groovy
+++ src/main/groovy/app/Main.groovy
@@ -15,62 +15,22 @@ import grails.gorm.transactions.Transactional
 
 def configuration = [
     'hibernate.hbm2ddl.auto':'create-drop',
-    'hibernate.dialect': 'org.hibernate.dialect.Comdb2Dialect',
-    'dataSource.url': 'jdbc:comdb2://localhost/testdb',
-    'dataSource.driverClassName': 'com.bloomberg.comdb2.jdbc.Driver',
+    'dataSource.url': 'jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE',
+    'dataSource.driverClassName': 'org.h2.Driver',
     'dataSource.logSql': 'true',
     //'dataSource.formatSql': 'true'
 ]
 
 
-def entities = [Widget, User, Soup, Thought]
+def entities = [User]
 HibernateDatastore datastore = new HibernateDatastore(configuration, *entities)
 
 
 @Transactional(isolation=Isolation.READ_COMMITTED)
 def doSomething() {
-    def w = new Widget(type: "pqr")
-    w.lastModified = new Date()
-    println w
-    //println w.save()
-
     def a = new User(name: "abc", title:"abct", widgets:[w])
     println a.save(flush:true)
 
-    a.name = "def"
-    println a.save()
-
-
-    def b = new User(name: 'ijk')
-    b.title = "title"
-    b.save(flush:true)
-    println b
-
-    def c = new User(name: 'mno', title: "king")
-    println "> for delete ${c.save(flush: true)}"
-    
-
-    println "> test deletion"
-    println "> byId:${User.get(c.id)}, byName:${User.findByTitle('king')}"
-    println "> delete ${User.findByName('mno').delete(flush:true)}"
-    println "> find again ${User.get(c.id)}"
-    
-    println "done"
-    
-    
-    Soup soup = new Soup(brand: 'a', product: 'b', flavor: 'c', live:true)
-    println "> Saving soup: ${soup.save(flush:true)}"
-    
-    Thought th1 = new Thought(uuid: 7, rating: 2, soup: soup)
-    println "> Saving thought 1: ${th1.save(flush:true)}"
-    
-    Thought th2 = new Thought(uuid: 7, rating: 4, soup: soup)
-    println "> Saving thought 2: ${th2.save(flush:true)}"
-    
-    
-    println "> Soup thoughts A: ${Thought.findBySoup(soup)}"
-    
-    println "> Soup thoughts B: ${Soup.get(soup.id).thoughts}"
 }
 
 datastore.withNewSession {doSomething()}
